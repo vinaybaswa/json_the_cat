58b996b38eb1964f0c931535eb3ebd3262a7714d
Vinay Baswa <vinaybaswa@gmail.com>
2020-08-28 13:55:58 +0000

    refactored and moved user facing code to index.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,23 +1,30 @@
 const request = require('request');
 
-const url = 'https://api.thecatapi.com/v1/breeds/search?q=' + process.argv[2];
-
-request(url, (error, response, body) => {
-  console.log('error:', error);
-  console.log('statusCode:', response && response.statusCode);
-  const data = JSON.parse(body);
-
-  if (response.statusCode === 200) {
-    if (data.length === 0) {
-      console.log("Requsted breed not found");
+const fetchBreedDescription = function(breedName, callback) {
+  const url = 'https://api.thecatapi.com/v1/breds/search?q=' + breedName;
+   request(url, (error, response, body) => {
+  //   console.log('error:', error);
+  //   console.log('statusCode:', response && response.statusCode);
+    const data = JSON.parse(body);
+    
+    if (response.statusCode === 200) {
+      if (data.length !== 0) {
+      callback(null, data[0].description)
+      } else {
+      callback(null,"Breed not found" )
+      }
     } else {
-      console.log(data[0].description);
+     callback(error, null)
     }
-  } else {
-    console.log("request failed, please check if url is correct");
-  }
+    
+  });
+  
+};
 
-});
+module.exports = { fetchBreedDescription };
+
+
+
 
 
 
